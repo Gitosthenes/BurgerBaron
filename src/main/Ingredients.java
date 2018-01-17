49d64fe5a84b8742a4bb237e0ad6bbc51436d92d
NegroKetchum <Negro.Ketchum.GitHub@gmail.com>
2018-01-16 16:50:02 -0800

    Add Enum class for ingredients.

---
+++ src/main/Ingredients.java
@@ -0,0 +1,29 @@
+package main;
+
+/**
+ * List of all possible ingredients when constructing burger,
+ * in order from bottom of burger, to top.
+ * 
+ * @author Alex Bledsoe
+ * @version Jan 16, 2018
+ */
+public enum Ingredients {
+
+	BUN,
+	KETCHUP,
+	MUSTARD,
+	MUSHROOMS,
+	BEEF_PATTY,
+	CHICKEN_PATTY,
+	VEGGIE_PATTY,	
+	CHEDDAR,
+	MOZZARELLA,
+	PEPPERJACK,
+	ONIONS,
+	TOMATO,
+	LETTUCE,
+	BARON_SAUCE,
+	MAYONNAISE,
+	//Top bun goes here.
+	PICKLE
+}
