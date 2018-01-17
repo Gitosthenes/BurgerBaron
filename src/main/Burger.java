a09604f641c75d05c14e78f064bb24e0043bccb6
Ben Yuen <benny08y@gmail.com>
2018-01-16 21:11:21 -0800

    Added empty Burger class.

---
+++ src/main/Burger.java
@@ -0,0 +1,90 @@
+package main;
+
+/**
+ * 
+ * @author Benjamin Yuen
+ * @version Jan 16, 2018
+ */
+public class Burger {
+	 
+ 	private MyStack myStack;
+
+	/**
+	 * A constructor that initializes a Burger with only a bun and
+	 * patty if theWorks is false and a Baron Burger if theWorks is true.
+	 * 
+	 * @param theWorks determines if the Burger is theWorks or not
+	 */
+	public Burger (boolean theWorks) {
+		
+	}
+
+	/**
+	 * Changes all patties in the Burger to the pattyType.
+	 * 
+	 * @param thePattyType the patty type that will change all patties 
+	 */
+	public void changePatties(String thePattyType) {
+		
+	}
+	
+	/**
+	 * Adds one patty to the Burger up to the maximum of 3. 
+	 */
+	public void addPatty() {
+		
+	}
+
+	/**
+	 * Removes one patty from the Burger down to the minimum of 1. 
+	 */
+	public void removePatty() {
+		
+	}
+	
+	/**
+	 * Adds all items of the type to the Burger in the proper locations. 
+	 * 
+	 * @param theType item that will be added to the Burger
+	 */
+	public void addCategory(String theType) {
+		
+	}
+	
+	/**
+	 * Removes all items of the type from the Burger. 
+	 * 
+	 * @param theType item that will be remove to the Burger
+	 */
+	public void removeCategory(String theType) {
+		
+	}
+	
+	/**
+	 * Adds the ingredient type to the Burger in the proper location.
+	 * 
+	 * @param theType ingredient that will be added to the Burger
+	 */
+	public void addIngredient(String theType) {
+		
+	}
+	
+	/**
+	 * Removes the ingredient type from the Burger.
+	 * 
+	 * @param theType ingredient that will be remove to the Burger
+	 */
+	public void removeIngredient(String theType) {
+		
+	}
+	
+	/**
+	 * Converts the Burger to a String for display.
+	 */
+	public String toString() {
+		
+		return null;		
+	}
+	
+	
+}
