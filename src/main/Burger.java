b70150186e80c60537077c6bc359987dbf4f3f26
AlexBledsoe <alexrbledsoe@gmail.com>
2018-01-18 00:19:27 -0800

    Flesh out Burger class and tweak classpath to work with java 8 and 9.
    Added fields (a top and bottom stack).
    Wrote constructor (with helper methods), changePatties and addPatty
    methods.
    Modified classpath to use system JRE instead of a specific one. This
    should allow for java 8 and 9 compatibility.

--- src/main/Burger.java
+++ src/main/Burger.java
@@ -7,16 +7,32 @@ package main;
  */
 public class Burger {
 	 
- 	private MyStack myStack;
+	/** The top half of the burger, usually a bun and veggies. */
+ 	private MyStack<Ingredients> myBurgerTop;
+ 	
+ 	/** The bottom half of the burger, usually a bun, patties and cheese. */
+ 	private MyStack<Ingredients> myBurgerBottom;
+ 	
+ 	/** Keeps track of the number of patties on the burger. */
+ 	private int myPattyCount;
 
 	/**
 	 * A constructor that initializes a Burger with only a bun and
 	 * patty if theWorks is false and a Baron Burger if theWorks is true.
 	 * 
-	 * @param theWorks determines if the Burger is theWorks or not
+	 * @param theWorks determines if the Burger is a Baron Burger or not.
 	 */
 	public Burger (boolean theWorks) {
+		myBurgerTop = new MyStack<>();
+		myBurgerBottom = new MyStack<>();
+			
+		if (theWorks) {
+			makeBaron();
+		} else {
+			makeBasic();
+		}
 		
+		myPattyCount++;
 	}
 
 	/**
@@ -25,14 +41,39 @@ public class Burger {
 	 * @param thePattyType the patty type that will change all patties 
 	 */
 	public void changePatties(String thePattyType) {
-		
+		Ingredients patty;
+		if ("Chicken".endsWith(thePattyType)) {
+			patty = Ingredients.CHICKEN_PATTY;
+		} else {
+			patty = Ingredients.VEGGIE_PATTY;
+		}
+		MyStack<Ingredients> cheeseHolder = new MyStack<>();
+		int currentPattyCount = myPattyCount;
+		while (currentPattyCount > 0) {
+			if (myBurgerBottom.peek() != Ingredients.BEEF_PATTY) {
+				cheeseHolder.push(myBurgerBottom.pop());
+			} else {
+				myBurgerBottom.pop();
+				currentPattyCount--;
+			}
+		}
+		myBurgerBottom.push(patty);
+		currentPattyCount++;
+		while (!cheeseHolder.isEmpty()) {
+			myBurgerBottom.push(cheeseHolder.pop());
+		}
+		while (currentPattyCount < myPattyCount) {
+			myBurgerBottom.push(cheeseHolder.pop());
+			currentPattyCount++;
+		}
 	}
 	
 	/**
 	 * Adds one patty to the Burger up to the maximum of 3. 
 	 */
 	public void addPatty() {
-		
+		myBurgerBottom.push(Ingredients.BEEF_PATTY);
+		myPattyCount++;
 	}
 
 	/**
@@ -81,10 +122,43 @@ public class Burger {
 	/**
 	 * Converts the Burger to a String for display.
 	 */
+	@Override
 	public String toString() {
 		
 		return null;		
 	}
 	
+	/**
+	 * Builds a standard Baron Burger.
+	 * Constructed in two halves to make adding and changing patties easier.
+	 */
+	private void makeBaron() {
+		myBurgerBottom.push(Ingredients.BUN);
+		myBurgerBottom.push(Ingredients.KETCHUP);
+		myBurgerBottom.push(Ingredients.MUSTARD);
+		myBurgerBottom.push(Ingredients.MUSHROOMS);
+		myBurgerBottom.push(Ingredients.BEEF_PATTY);
+		myBurgerBottom.push(Ingredients.CHEDDAR);
+		myBurgerBottom.push(Ingredients.MOZZARELLA);
+		myBurgerBottom.push(Ingredients.PEPPERJACK);
+		
+		myBurgerTop.push(Ingredients.PICKLE);
+		myBurgerTop.push(Ingredients.BUN);
+		myBurgerTop.push(Ingredients.MAYONNAISE);
+		myBurgerTop.push(Ingredients.BARON_SAUCE);
+		myBurgerTop.push(Ingredients.LETTUCE);
+		myBurgerTop.push(Ingredients.TOMATO);
+		myBurgerTop.push(Ingredients.ONIONS);
+	}
 	
+	/**
+	 * Builds a basic burger (buns and patty).
+	 * Constructed in two halves to make adding and changing patties easier.
+	 */
+	private void makeBasic() {
+		myBurgerBottom.push(Ingredients.BUN);
+		myBurgerBottom.push(Ingredients.BEEF_PATTY);
+		
+		myBurgerTop.push(Ingredients.BUN);
+	}
 }
